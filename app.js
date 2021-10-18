551f3bbbf815e4da8d9d7a4a27b38b2afb06f5b5
Leonardo Ramos <leorrc96@gmail.com>
2021-10-18 15:02:19 -0400

    update(app.js): iniciar el browser desde app.js, si hay error al iniciar el browser, cerrar el servidor

--- app.js
+++ app.js
@@ -1,36 +1,41 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+const whatsAppRoutes = require("./routes/whatsapp.route");
+const { initBrowser } = require("./whats-app");
+
+// Si hay error al iniciar el browser, cerrar el proceso de node.
+initBrowser().catch((err) => {
+  console.log(err);
+  process.exit(1);
+});
 
 const app = express();
-const port = process.env.PORT || 3002
-const whatsAppRoutes = require('./routes/whatsapp.route');
-/* const whatsapp = require('./whats-app'); */
+const port = process.env.PORT || 3002;
 
 //CORS
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
 });
 
-app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
 
 // serving static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 /* Para las imagenes */
-app.use(express.static(path.join(__dirname, 'public/img')))
+app.use(express.static(path.join(__dirname, "public/img")));
 
 app.use(express.json());
 
-app.use('/whatsapp', whatsAppRoutes);
+app.use("/whatsapp", whatsAppRoutes);
 
-
-app.listen(port,()=>{
-    console.log(`Listen in Port ${port}`);
-})
-
-
-
+app.listen(port, () => {
+  console.log(`Listen in Port ${port}`);
+});
